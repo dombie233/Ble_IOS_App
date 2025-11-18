3865280a3c29b9133614ad840315ae5766eb21d3
dombie233 <dominik.learning2003@gmail.com>
2025-11-18 12:37:32 +0100

    Initial Commit

---
+++ HeartRateMonitorTests/HeartRateMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMonitorTests.swift
+//  HeartRateMonitorTests
+//
+//  Created by needcode on 18/11/2025.
+//
+
+import Testing
+@testable import HeartRateMonitor
+
+struct HeartRateMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
